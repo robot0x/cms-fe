91d8de53f5ed313605c5d38ba3bd5d5567c005b9
lyf <1601942611@qq.com>
2017-02-07 11:42:08 +0800

    归档

--- src/views/Author.vue
+++ src/views/Author.vue
@@ -1,40 +1,18 @@
 <template>
-    <div class="page-author">
-      <el-row>
-         <el-col :span="4">
-            <el-menu @select="select" theme="dark" :default-active="defaultActive">
-              <el-menu-item :index="index | calcIndex" v-for="(item, index) in items">
-                {{item.name}}（{{item.count}}）
-              </el-menu-item>
-              <!-- <el-menu-item index="1-1">8023wei (218)</el-menu-item>
-              <el-menu-item index="1-2">949815562 (1)</el-menu-item>
-              <el-menu-item index="1-3">a574427792 (152)</el-menu-item>
-              <el-menu-item index="1-4">Anyuna (4)</el-menu-item>
-              <el-menu-item index="1-5">august (6)</el-menu-item>
-              <el-menu-item index="1-6">chengmingjia (2)</el-menu-item>
-              <el-menu-item index="1-7">chenguanglin (153)</el-menu-item>
-              <el-menu-item index="1-8">chenyisahn (24)</el-menu-item>
-              <el-menu-item index="1-9">cuicui (11)</el-menu-item>
-              <el-menu-item index="1-10">dayuan (477)</el-menu-item>
-              <el-menu-item index="1-11">diamond (584)</el-menu-item>
-              <el-menu-item index="1-12">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-13">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-14">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-15">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-16">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-17">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-18">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-19">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-20">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-21">dongxu (207)</el-menu-item>
-              <el-menu-item index="1-22">dongxu (207)</el-menu-item> -->
-          </el-menu>
-         </el-col>
-         <el-col :span="20">
-           <data-grid class="data-grid" :input="query"></data-grid>
-         </el-col>
-      </el-row>
-    </div>
+  <div class="page-author">
+    <el-row>
+       <el-col :span="4">
+          <el-menu @select="select" theme="dark" :default-active="defaultActive">
+            <el-menu-item :index="index | calcIndex" v-for="(item, index) in items">
+              {{item.name}}（{{item.count}}）
+            </el-menu-item>
+        </el-menu>
+       </el-col>
+       <el-col :span="20">
+         <data-grid class="data-grid" :input="query"></data-grid>
+       </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
 import DataGrid from '../components/DataGrid'
--- src/views/Monthly.vue
+++ src/views/Monthly.vue
@@ -1,73 +1,17 @@
 <template>
-     <div class="page-monthly">
-       <el-row>
-          <el-col :span="4">
-             <el-menu :default-openeds="defaultOpeneds"	:default-active="defaultActive" class="el-menu-vertical-demo" theme="dark" @select="select">
-               <el-submenu v-for="(item, index) in items" :index="index | calcIndex">
-                 <template slot="title">{{ item.year }}年（{{item.count}}）</template>
-                 <el-menu-item :index="index | calcIndex(i)" v-for="(month, i) in item.months"> {{ month | monthFormat }} </el-menu-item>
-               </el-submenu>
-               <!-- <el-submenu index="1">
-                   <template slot="title">2017年（200）</template>
-                   <el-menu-item index="1-1">一月</el-menu-item>
-                   <el-menu-item index="1-2">二月</el-menu-item>
-                   <el-menu-item index="1-3">三月</el-menu-item>
-                   <el-menu-item index="1-4">四月</el-menu-item>
-                   <el-menu-item index="1-5">五月</el-menu-item>
-                   <el-menu-item index="1-6">六月</el-menu-item>
-                   <el-menu-item index="1-7">七月</el-menu-item>
-                   <el-menu-item index="1-8">八月</el-menu-item>
-                   <el-menu-item index="1-9">九月</el-menu-item>
-                   <el-menu-item index="1-10">十月</el-menu-item>
-                   <el-menu-item index="1-11">十一月</el-menu-item>
-                   <el-menu-item index="1-12">十二月</el-menu-item>
-               </el-submenu>
-
-               <el-submenu index="2">
-                   <template slot="title">2016年（3890）</template>
-                   <el-menu-item index="2-1">一月</el-menu-item>
-                   <el-menu-item index="2-2">二月</el-menu-item>
-                   <el-menu-item index="2-3">三月</el-menu-item>
-                   <el-menu-item index="2-4">四月</el-menu-item>
-                   <el-menu-item index="2-5">五月</el-menu-item>
-                   <el-menu-item index="2-6">六月</el-menu-item>
-                   <el-menu-item index="2-7">七月</el-menu-item>
-                   <el-menu-item index="2-8">八月</el-menu-item>
-                   <el-menu-item index="2-9">九月</el-menu-item>
-                   <el-menu-item index="2-10">十月</el-menu-item>
-                   <el-menu-item index="2-11">十一月</el-menu-item>
-                   <el-menu-item index="2-12">十二月</el-menu-item>
-               </el-submenu>
-
-               <el-submenu index="3">
-                   <template slot="title">2015年（4120）</template>
-                   <el-menu-item index="3-1">一月</el-menu-item>
-                   <el-menu-item index="3-2">二月</el-menu-item>
-                   <el-menu-item index="3-3">三月</el-menu-item>
-                   <el-menu-item index="3-4">四月</el-menu-item>
-                   <el-menu-item index="3-5">五月</el-menu-item>
-                   <el-menu-item index="3-6">六月</el-menu-item>
-                   <el-menu-item index="3-7">七月</el-menu-item>
-                   <el-menu-item index="3-8">八月</el-menu-item>
-                   <el-menu-item index="3-9">九月</el-menu-item>
-                   <el-menu-item index="3-10">十月</el-menu-item>
-                   <el-menu-item index="3-11">十一月</el-menu-item>
-                   <el-menu-item index="3-12">十二月</el-menu-item>
-               </el-submenu>
-
-               <el-submenu index="4">
-                   <template slot="title">2014年（899）</template>
-                   <el-menu-item index="4-10">十月</el-menu-item>
-                   <el-menu-item index="4-11">十一月</el-menu-item>
-                   <el-menu-item index="4-12">十二月</el-menu-item>
-               </el-submenu> -->
-           </el-menu>
-          </el-col>
-          <el-col :span="20">
-            <data-grid class="data-grid" :input="query"></data-grid>
-          </el-col>
-       </el-row>
-     </div>
+ <div class="page-monthly">
+   <el-row>
+      <el-col :span="4">
+         <el-menu :default-openeds="defaultOpeneds"	:default-active="defaultActive" class="el-menu-vertical-demo" theme="dark" @select="select">
+           <el-submenu v-for="(item, index) in items" :index="index | calcIndex">
+             <template slot="title">{{ item.year }}年（{{item.count}}）</template>
+             <el-menu-item :index="index | calcIndex(i)" v-for="(month, i) in item.months"> {{ month | monthFormat }}</el-menu-item>
+           </el-submenu>
+       </el-menu>
+      </el-col>
+      <el-col :span="20"> <data-grid class="data-grid" :input="query"></data-grid> </el-col>
+   </el-row>
+ </div>
 </template>
 
 <script>
