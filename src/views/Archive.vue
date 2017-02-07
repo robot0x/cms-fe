23d6d3936e398d187fc25fe6307f2d5e73866942
lyf <1601942611@qq.com>
2017-02-07 11:51:36 +0800

    归档

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -9,7 +9,6 @@
        <keep-alive>
          <router-view></router-view>
        </keep-alive>
-       <!-- <router-view></router-view> -->
      </transition>
    </div>
 </template>
@@ -17,41 +16,34 @@
 <script>
 import Panel from '../components/Panel'
 export default {
-  components: {
-    Panel
-  },
+  components: { Panel },
   data () {
-    return {
-      archive: 'monthly'
-    }
+    return { archive: 'monthly' }
   },
-  watch: {
-     // 如果路由有变化，会再次执行该方法
-     // 刷新页面导致的路由变化，并不会触发，所以必须要监听 created 钩子，才能考虑到所有的情况
-     '$route': 'routeChange'
-  },
+  // watch: {
+  //    // 如果路由有变化，会再次执行该方法
+  //    // 刷新页面导致的路由变化，并不会触发，所以必须要监听 created 钩子，才能考虑到所有的情况
+  //    '$route': 'routeChange'
+  // },
   methods: {
     route (name) {
       this.$router.replace({name: name})
     },
     routeChange () {
-      const path = this.$route.path
-      if(path.indexOf('author') !== -1){
-        this.archive = 'author'
-      }else{
-        this.archive = 'monthly'
-      }
+      const archive = this.$route.path.indexOf('author') !== -1 ? 'author' : 'monthly'
+      this.archive = archive
     }
   },
   // 不能只在created中监听routeChange。不然的话，在地址栏输入地址，页面无刷新的话，就不会走Archive的created的钩子了
   // 即虽然路由变了，但是created并不执行
-  created () {
+  // 改为activated钩子即可，用不着监听 $route
+  activated () {
     // 如果是keep-alive的话created其实只执行一次
     this.routeChange()
   }
 }
 </script>
-<style lang="scss" scoped>
+<style scoped>
   .route-box {
     background-color: #324057;
     padding: 20px 0;
