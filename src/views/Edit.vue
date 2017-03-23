abb657b27faf8c18e881993ad84e5a9d7fd669f5
lyf <1601942611@qq.com>
2017-03-23 14:54:25 +0800

    update

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -85,19 +85,6 @@
                 <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
                 <div class="el-upload__tip" slot="tip">只能上传jpg/jpeg/png/gif文件，且不超过500kb</div>
               </el-upload>
-              <!-- <el-upload
-                action="http://z.diaox2.com/view/app/upfornewcms.php"
-                type="drag"
-                accept="image/*"
-                :data="{'id':id}"
-                :before-upload="handleBeforeUpload"
-                :on-success="handleSuccess"
-                :on-error="handleError"
-                :multiple="true">
-                <i class="el-icon-upload"></i>
-                <div class="el-dragger__text">将文件拖到此处，或<em>点击上传</em></div>
-                <div class="el-upload__tip" slot="tip">只能上传jpg/jpeg/png/gif文件，且不超过500kb</div>
-              </el-upload> -->
             </el-form-item>
           </el-col>
           <el-col :span="14">
